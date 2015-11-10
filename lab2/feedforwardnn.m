50b7858ffd6dc6ff9160253e2579e7d1f5ed4e5c
memeda <readonlyfile@hotmail.com>
2015-11-10 10:28:20 +0800

    change at 20151110 ; change to NLL , mini-batch

--- lab2/feedforwardnn.m
+++ lab2/feedforwardnn.m
@@ -15,7 +15,7 @@ function [  ] = feedforwardnn( )
     OUTPUT_NODE_NUM = 4 ;
     INPUT_NODE_NUM = 3 ;
     
-    ITE_NUM = 40 ;
+    ITE_NUM = 80 ;
     
     % 学习率小一点似乎更加稳定，反正不要取1就是了
     OUTPUT_ALPHA = 0.01 ;
@@ -23,51 +23,69 @@ function [  ] = feedforwardnn( )
     
     hidden_w = rand(HIDDEN_NODE_NUM,INPUT_NODE_NUM) ; % every row is a vector for a hidden node ; every col is corresponding to a input .
     hidden_b = rand(HIDDEN_NODE_NUM,1) ; % every row is the bias of a hidden node
-    
+    hidden_w_gradient = zeros(HIDDEN_NODE_NUM , INPUT_NODE_NUM) ;
+    hidden_b_gradient = zeros(HIDDEN_NODE_NUM , 1) ;
     
     output_w = rand(OUTPUT_NODE_NUM , HIDDEN_NODE_NUM) ;
     output_b = rand(OUTPUT_NODE_NUM , 1) ;
+    output_w_gradient = zeros(OUTPUT_NODE_NUM , HIDDEN_NODE_NUM) ;
+    output_b_gradient = zeros(OUTPUT_NODE_NUM , 1) ;
     
     [ training_x , training_y , testing_x ,  testing_y ] = generate_data(false) ;
     
     TRAINING_SIZE = size(training_x , 1) ; % get row of trainning_x , which is the nums of trainning data
     TESTING_SIZE = size(testing_x , 1) ;
+    
+    BATCH_SIZE = 10 ;
+    
+    
     for ite = 1 : ITE_NUM 
-        for training_idx = 1 : TRAINING_SIZE 
-          
-         instance_x = training_x(training_idx,:)' ;
-         instance_y = OUTPUT_LABEL2ID(training_y(training_idx)) ;
+        randidx = randperm(TRAINING_SIZE) ;
+        for instance_ite_idx = 1 : TRAINING_SIZE 
+         instance_idx = randidx(instance_ite_idx) ;
+         instance_x = training_x(instance_idx,:)' ;
+         instance_y = OUTPUT_LABEL2ID(training_y(instance_idx)) ;
          
          %% feed forward
          hidden_layer_out = sigmoid(hidden_w * instance_x + hidden_b) ;
          output_layer_out = softmax(output_w * hidden_layer_out + output_b) ;
          
          %% back propagation
-         true_output = zeros(OUTPUT_NODE_NUM , 1) ;
-         true_output(instance_y) = 1 ;
          
          %% MSE
-         %%output_layer_delta = 2 * ( output_layer_out - true_output) .* output_layer_out .* ( 1 - output_layer_out) ;
+         %true_output = zeros(OUTPUT_NODE_NUM , 1) ;
+         %true_output(instance_y) = 1 ;
+         %output_layer_delta = 2 * ( output_layer_out - true_output) .* output_layer_out .* ( 1 - output_layer_out) ;
          
-         %% NLL - one possible pattern
-         %%output_layer_delta = zeros(OUTPUT_NODE_NUM , 1) ;
-         %%output_layer_delta(instance_y) = output_layer_out(instance_y) - 1 ;
-         %% NLL - another possible pattern
-         output_layer_delta = output_layer_out - true_output ;
+         %% NLL
+         diff_value = zeros(OUTPUT_NODE_NUM , 1) ;
+         diff_value(instance_y) = 1 ;
+         output_layer_delta = output_layer_out - diff_value ;
 
          
          hidden_layer_delta = (output_layer_delta' * output_w )' .* hidden_layer_out .* (1 - hidden_layer_out) ;
          
-         
          % 如果加入正则，就又成为25% 了！！
+         output_w_gradient = output_w_gradient + repmat(output_layer_delta , 1 , HIDDEN_NODE_NUM) .* repmat(hidden_layer_out' , OUTPUT_NODE_NUM , 1)  ;
+         hidden_w_gradient = hidden_w_gradient + repmat(hidden_layer_delta , 1 , INPUT_NODE_NUM) .* repmat(instance_x' , HIDDEN_NODE_NUM , 1)  ;
          
-         output_w_gradient = repmat(output_layer_delta , 1 , HIDDEN_NODE_NUM) .* repmat(hidden_layer_out' , OUTPUT_NODE_NUM , 1)  ;
+         output_b_gradient = output_b_gradient + output_layer_delta ;
+         hidden_b_gradient = hidden_b_gradient + hidden_layer_delta ;
          
-         hidden_w_gradient = repmat(hidden_layer_delta , 1 , INPUT_NODE_NUM) .* repmat(instance_x' , HIDDEN_NODE_NUM , 1)  ;
-         
-         output_w = output_w - OUTPUT_ALPHA * output_w_gradient ;
-         hidden_w = hidden_w - HIDDEN_ALPHA * hidden_w_gradient ;
-
+         % if batch finished , or went to the last , then UPDATE
+         if mod(instance_ite_idx , BATCH_SIZE) == 0 || instance_ite_idx == TRAINING_SIZE 
+            output_w = output_w - OUTPUT_ALPHA * output_w_gradient ;
+            hidden_w = hidden_w - HIDDEN_ALPHA * hidden_w_gradient ;
+            
+            output_b = output_b - OUTPUT_ALPHA * output_b_gradient ;
+            hidden_b = hidden_b - HIDDEN_ALPHA * hidden_b_gradient ;
+            % clear gradient
+            output_w_gradient = zeros(OUTPUT_NODE_NUM , HIDDEN_NODE_NUM) ;
+            hidden_w_gradient = zeros(HIDDEN_NODE_NUM , INPUT_NODE_NUM) ;
+            
+            output_b_gradient = zeros(OUTPUT_NODE_NUM , 1) ;
+            hidden_b_gradient = zeros(HIDDEN_NODE_NUM , 1) ;
+         end
          %%
          
         end
